913c71a60b301cac84843524332498f74ca1cf1a
Tharaka_Madhusanka <tharakalearn@gmail.com>
2025-03-23 21:12:09 +0800

    Modified to run FE on PORT 4200

--- Aspire.Hosting.AppHost.AppHost/Program.cs
+++ Aspire.Hosting.AppHost.AppHost/Program.cs
@@ -5,6 +5,7 @@ var apiService = builder.AddProject<Projects.Aspire_Hosting_AppHost_ApiService>(
 builder.AddNpmApp("angular", "../Aspire.Angular")
     .WithReference(apiService)
     .WaitFor(apiService)
+    .WithHttpEndpoint(port: 4200, env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
